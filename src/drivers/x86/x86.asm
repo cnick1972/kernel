6fefe81955990a321071de10624e96f517d616bf
Chris Nicholson <chris@cnick.org.uk>
2025-11-01 18:49:42 +0000

    Refactor x86 assembly I/O functions to improve clarity and consistency

--- src/drivers/x86/x86.asm
+++ src/drivers/x86/x86.asm
@@ -1,62 +1,69 @@
+[bits 32]
+[section .text]
+
+;void        ASMCALL x86_outb(uint16_t port, uint8_t value)
 global x86_outb
 x86_outb:
-    [bits 32]
     mov dx, [esp + 4]
     mov al, [esp + 8]
     out dx, al
     ret
 
+;uint8_t     ASMCALL x86_inb(uint16_t port)
 global x86_inb
 x86_inb:
-    [bits 32]
     mov dx, [esp + 4]
     xor eax, eax
     in al, dx
     ret
 
-
+;void        ASMCALL x86_outw(uint16_t port, uint16_t value
 global x86_outw
 x86_outw:
-    [bits 32]
     mov dx, [esp + 4]
     mov ax, [esp + 8]
     out dx, ax
     ret
 
+;uint16_t    ASMCALL x86_inw(uint16_t port)
 global x86_inw
 x86_inw:
-    [bits 32]
     mov dx, [esp + 4]
     xor eax, eax
     in ax, dx
     ret
 
+;void        ASMCALL x86_outl(uint16_t port, uint32_t value)
 global x86_outl
 x86_outl:
-    [bits 32]
     mov dx, [esp + 4]
     mov eax, [esp + 8]
     out dx,eax
     ret
 
+;uint32_t    ASMCALL x86_inl(uint16_t port)
+
 global x86_inl
 x86_inl:
-    [bits 32]
     mov dx, [esp + 4]
     xor eax, eax
     in eax, dx
     ret
 
+;void        ASMCALL x86_Panic()
 global x86_Panic
 x86_Panic:
     cli
     hlt
 
+
+;uint8_t     ASMCALL x86_EnableInterrupts()
 global x86_EnableInterrupts
 x86_EnableInterrupts:
     sti
     ret
 
+;uint8_t     ASMCALL x86_DisableInterrupts()
 global x86_DisableInterrupts
 x86_DisableInterrupts:
     cli
@@ -71,18 +78,20 @@ x86_InvalidatePage:
     ret
 
 
-;void        ASMCALL x86_ReloadPageDirectory();
+;void        ASMCALL x86_ReloadPageDirectory()
 global x86_ReloadPageDirectory
 x86_ReloadPageDirectory:
-    cli
+    cli             ; disable interrupts, we don't want to be interrupted here
     mov eax, cr3
     mov cr3, eax
-    sti
+    sti             ; re-enable interrupts   
     ret
 
+
+;uint32_t    ASMCALL x86_CR2_register()
 global x86_CR2_register
 x86_CR2_register:
-    cli
-    mov eax, cr2
-    sti
+    cli                 ; disable interrupts
+    mov eax, cr2        ; read CR2 into EAX
+    sti                 ; re-enable interrupts
     ret
