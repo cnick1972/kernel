2796b25833ba6582dbeeaae1a7d2b3c1683be15b
Chris Nicholson <chris@cnick.org.uk>
2025-11-01 15:56:31 +0000

    Refactor PageFaultHandler to improve memory allocation and initialization

--- src/system/memory/vmm.c
+++ src/system/memory/vmm.c
@@ -1,5 +1,6 @@
 #include <meminit.h>
 #include <stdbool.h>
+#include <memory.h>
 #include <stdio.h>
 #include <isr.h>
 #include <x86.h>
@@ -130,13 +131,14 @@ void PageFaulthandler(Registers* regs)
     page_directory_t pd = (page_directory_t) &PageDirectoryVirtualAddress;
 
     // do i have a page directory entry for the memory requested?
-    uint32_t directoryEntry = cr2 / 1024 / 4096;
+    uint32_t directoryEntry = cr2 >> 22;
+    page_table_t pt;
 
     if(!get_present_from_pde(pd[directoryEntry]))
     {
         
         // allocate a memory block for the required pde
-        void* newPage = (void*)allocate_physical_page();
+        uintptr_t newPage = allocate_physical_page();
         uint32_t pde = make_page_directory_entry((void*) newPage, 
                         FOUR_KB, 
                         false, 
@@ -147,23 +149,28 @@ void PageFaulthandler(Registers* regs)
 
         pd[directoryEntry] = pde;
 
-        page_table_t pt = (page_table_t) page_table_virtual_address(directoryEntry);
+        pt = (page_table_t) page_table_virtual_address(directoryEntry);
+        memset(pt, 0, PAGE_SIZE_BYTES);
+    }
+    else
+    {
+        pt = (page_table_t) page_table_virtual_address(directoryEntry);
+    }
 
-        uint32_t tbentry = (cr2 >> 12) & 0x3ff;
+    uint32_t tbentry = (cr2 >> 12) & 0x3ff;
 
-        if(!get_present_from_pte(pt[tbentry]))
-        {
+    if(!get_present_from_pte(pt[tbentry]))
+    {
 
-            newPage = (void*)allocate_physical_page();
-        
-            pt[tbentry] = make_page_table_entry(newPage, 
-                            false, 
-                            false, 
-                            false, 
-                            SUPERVISOR, 
-                            READ_WRITE, 
-                            true);
-        }
+        uintptr_t newPage = allocate_physical_page();
+    
+        pt[tbentry] = make_page_table_entry((void*)newPage, 
+                        false, 
+                        false, 
+                        false, 
+                        SUPERVISOR, 
+                        READ_WRITE, 
+                        true);
     }
     x86_ReloadPageDirectory();
     //kprintf("In handler 0x%08x\n", cr2);
